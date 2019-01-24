86097a9bd2f5a408bd4c631624321c4ab6ff85f5
antoniaantonyuk <ansemakova@gmail.com>
2019-01-23 19:54:05 -0500

    substracted method

--- src/Calculaor.java
+++ src/Calculaor.java
@@ -2,7 +2,10 @@ public class Calculaor {
     public static double add(double num1, double num2){
         return num1+num2;
     }
-    public static double substract(double num1, double num2){
+    public static double Multiple(double num1, double num2){
+        return num1*num2;
+    }
+    public static double substraction (double num1, double num2){
         return num1-num2;
     }
 }
